67c31c239ea6e9fdf0df78b99a28c59b5cd86e21
Felipe Moura (Mac Love) <felipe.mourabarbosa@hotmail.com>
2025-12-01 00:48:41 -0300

    first commit

---
+++ SubModule.cs
@@ -0,0 +1,34 @@
+﻿using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace Bannerlord.LordLife
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            Debug.Print("[LordLife] SubModule carregado.");
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+            Debug.Print("[LordLife] SubModule descarregado.");
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+            InformationManager.DisplayMessage(
+                new InformationMessage(
+                    "LordLife carregado com sucesso!",
+                    Colors.Green
+                )
+            );
+        }
+    }
+}
